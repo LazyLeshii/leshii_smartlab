d37ae23063046a5533ac9cc6a011a2e5b62d5333
lazy_leshii <enotik.2005@list.ru>
2023-06-09 03:20:59 +0600

    Initial commit

---
+++ app/src/main/java/me/smartlab/leshii/Splash.java
@@ -0,0 +1,35 @@
+package me.smartlab.leshii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import me.smartlab.leshii.R;
+import me.smartlab.leshii.Session;
+
+public class Splash extends AppCompatActivity
+{
+    private static final int SPLASH_SCREEN_TIMEOUT = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent intent = new Intent(Splash.this, Session.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT * 1000);
+
+    }
+}
